258082cc8c0b6aa3db897c79dfeef46e851d24ac
Silent Web Studio <307634623+silentwebstudio@users.noreply.github.com>
2026-07-23 21:51:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
 
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    if (nav.classList.contains("active")) {
+        menuBtn.innerHTML = '<i class="fas fa-times"></i>';
+    } else {
+        menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+    }
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+        menuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.style.background =
+        window.scrollY > 50
+            ? "rgba(0,0,0,.95)"
+            : "rgba(0,0,0,.75)";
+});
